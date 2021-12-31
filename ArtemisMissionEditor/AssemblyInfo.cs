e20928e1460ced74db420f7320b52c1482a370bc
FishEvans <goti42@hotmail.com>
2021-12-31 21:19:04 +0000

    Add files via upload

---
+++ ArtemisMissionEditor/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TEST BUILD Artemis Mission Editor")]
+[assembly: AssemblyDescription("!!TEST BUILD!!\r\nComplete mission editor suite for Artemis SBS\r\n\r\nGet the latest version on Artemis Wiki:\r\nhttp://artemiswiki.pbworks.com/w/page/53389687/Mission_Editor\r\n\r\nUpdated for Artemis 2.8.0 by Russ Judge\r\n\r\nUpdated for Artemis 2.8.0 by Fish\r\n\r\nBug fixes and tweaking by David Wolfe, Alice and Gomeric\r\n\r\nUpdated for Artemis 2.5.1 and later by Dave Thaler, including contributions by Fish Evans, Scott Nalley, and Stoo Johnston\r\n\r\nReport bugs on the forum thread at: https://artemis.forumchitchat.com/post/artemis-mission-editor-8115862")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ARME Corporation :-)")]
+[assembly: AssemblyProduct("Artemis Mission Editor")]
+[assembly: AssemblyCopyright("Copyleft © 2012 Alexey Andronov")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("97dd1ca4-8f52-417f-9df1-0374bb3fb9c9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.8.00")]
